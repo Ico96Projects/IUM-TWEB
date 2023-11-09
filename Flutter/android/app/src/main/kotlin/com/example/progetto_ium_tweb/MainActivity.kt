7c237d702f97bdd638949597514ef70b044ea4a9
Ico96 <150274503+Ico96Projects@users.noreply.github.com>
2023-11-09 18:29:04 +0100

    flutter_3

---
+++ Flutter/android/app/src/main/kotlin/com/example/progetto_ium_tweb/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.progetto_ium_tweb
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Flutter/lib/Resources/utils.dart
@@ -0,0 +1,4 @@
+class Utils {
+    static const String ip = "172.18.123.248:8080/IUM_TWEB_war_exploded";
+    static String token = '';
+}
